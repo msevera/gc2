75d93c7af2e09129031253b1cff351f03b0f8aff
Mike Severa <michael.svr@gmail.com>
2024-10-11 12:21:32 +0200

    Updates

--- app.config.js
+++ app.config.js
@@ -1,6 +1,35 @@
-{
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const id = 'com.msevera.gc2';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return `${id}.dev`;
+  }
+
+  if (IS_PREVIEW) {
+    return `${id}.preview`;
+  }
+
+  return id;
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'gc (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'gc (Preview)';
+  }
+
+  return 'gc';
+};
+
+export default {
   "expo": {
-    "name": "gc2",
+    "name": getAppName(),
     "slug": "gc2",
     "version": "1.0.0",
     "orientation": "portrait",
@@ -14,7 +43,7 @@
     },
     "ios": {
       "supportsTablet": true,
-      "bundleIdentifier": "com.msevera.gc2"
+      "bundleIdentifier": getUniqueIdentifier()
     },
     "android": {
       "adaptiveIcon": {
